9e151ac55949c535f97040e50d1c5c809419d3ec
Minseo Kim <68639278+star14ms@users.noreply.github.com>
2024-06-19 07:28:13 -0400

    Create: Get Notes from Frequency Spectrogram

--- main_simplified_spec.py
+++ main_simplified_spec.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-from constant import NOTE_TO_KEY
+from constant import NOTE_TO_KEY, NOTES
 from modules.test import get_synth
 from modules.preprocess import get_audio_frequency_spectrogram
 import librosa
@@ -18,9 +18,7 @@ def synth(func):
 
 
 @synth
-def extract_notes(fs, spectrogram, sr, n_fft, hop_length, threshold, close_threshold, note_start_threshold):
-    frequencies = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
-    specidx_to_note = [librosa.hz_to_note(frequency).replace('♯', '#') for frequency in frequencies[2:]]
+def extract_notes(fs, spectrogram, sr, hop_length, threshold, close_threshold, note_start_threshold):
     times = librosa.frames_to_time(np.arange(spectrogram.shape[1]), sr=sr, hop_length=hop_length)
 
     frame_start = 30
@@ -28,20 +26,20 @@ def extract_notes(fs, spectrogram, sr, n_fft, hop_length, threshold, close_thres
     previous_indexes = set()
     pressed = set()
     
-    for i, pitches in enumerate(spectrogram[2:, frame_start:].T):
-        indexes_initial = set(np.where(pitches - previous_pitches > note_start_threshold)[0]) & set(np.where(pitches > threshold)[0])
-        previous_indexes = set(indexes_initial)
-        indexes = list(indexes_initial - previous_indexes)
+    for i, pitches in enumerate(spectrogram[:, frame_start:].T):
+        indexes = set(np.where(pitches - previous_pitches > note_start_threshold)[0]) & set(np.where(pitches > threshold)[0])
+        indexes = list(indexes - previous_indexes)
         
         # check if the note is continuous
         for j in range(len(indexes)-1, -1, -1):
-            if np.mean(spectrogram[i:i+10, indexes[j]]) < threshold - 10:
+            if np.mean(spectrogram[i:i+5, indexes[j]]) < threshold - 10:
                 indexes = np.delete(indexes, j)
 
         pressed = pressed.union(indexes)
-        notes = set([specidx_to_note[idx] for idx in indexes])
+        notes = set([NOTES[idx] for idx in indexes])
         keys = set([NOTE_TO_KEY[note] for note in notes])
         
+        previous_indexes = set(indexes)
         previous_pitches = pitches.copy()
 
         print(round(times[frame_start+i], 2), [round(pitches[idx], 0) for idx in indexes], [note for note in notes])
@@ -52,8 +50,8 @@ def extract_notes(fs, spectrogram, sr, n_fft, hop_length, threshold, close_thres
         time.sleep(0.2)
         
         for idx in pressed.copy():
-            if np.mean(spectrogram[i:i+5, idx]) < close_threshold:
-                key = NOTE_TO_KEY[specidx_to_note[idx]]
+            if spectrogram[i, idx] < close_threshold:
+                key = NOTE_TO_KEY[NOTES[idx]]
                 fs.noteoff(0, key)
                 pressed.remove(idx)
 
@@ -62,13 +60,13 @@ if __name__ == '__main__':
     audio_file = 'data/audio/Yiruma, (이루마) - River Flows in You [7maJOI3QMu0].webm' ### 수정하세요
     # audio_file = "data/Everything's Alright- Laura Shigihara- lyrics [nP-AAlZlCkM].m4a"
 
-    n_fft = 2048
+    n_fft = 8192
     threshold = -40
     close_threshold = -50
     note_start_threshold = 5
     win_length = 2048
-    hop_length = 512
+    hop_length = 1024
 
     spectrogram, sr = get_audio_frequency_spectrogram(audio_file, n_fft, hop_length)
-    extract_notes(spectrogram, sr, n_fft, hop_length, threshold, close_threshold, note_start_threshold)
+    extract_notes(spectrogram, sr, hop_length, threshold, close_threshold, note_start_threshold)
     
---
+++ modules/preprocess.py
@@ -0,0 +1,70 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+from constant import NOTE_FREQUENCIES
+from matplotlib.ticker import FixedLocator
+
+
+def get_frequency_spectrogram(audio_file, n_fft, hop_length, plot=False):
+    # Load the audio file
+    y, sr = librosa.load(audio_file)
+
+    # Generate the Short-Time Fourier Transform (STFT) of the audio
+    D = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length))
+
+    # Convert amplitude to decibels
+    DB = librosa.amplitude_to_db(D, ref=np.max) # Shape: [Frequency, Time]: dB
+
+    if plot:
+        # Display the spectrogram
+        plt.figure(figsize=(20, 12))
+        librosa.display.specshow(DB, sr=sr, x_axis='time', y_axis='log')
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Spectrogram')
+        plt.xlabel('Time')
+        plt.ylabel('Frequency')
+        plt.show()
+    
+    return DB, sr
+
+
+def optimize_spectrogram_best_represent_each_note(spectrogram, sr):
+    note_to_specindex = {}
+    n_fft = 2 * (spectrogram.shape[0] - 1)
+    frequencies = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
+    
+    for note_frequency in NOTE_FREQUENCIES:
+        note = librosa.note_to_hz(note_frequency)
+        note_index = np.abs(frequencies - note).argmin()
+        note_to_specindex[note_frequency] = note_index
+
+    print(spectrogram.shape)
+    spectrogram = np.take(spectrogram, list(note_to_specindex.values()), axis=0)
+    print(spectrogram.shape)
+        
+    return spectrogram
+
+
+def get_audio_frequency_spectrogram(audio_file, n_fft, hop_length):
+    spectrogram, sr = get_frequency_spectrogram(audio_file, n_fft, hop_length, plot=False)
+    # spectrogram = optimize_spectrogram_best_represent_each_note(spectrogram, sr)[:, :44*30]
+    
+    # # Display the spectrogram
+    # plt.figure(figsize=(15, 9))
+    # plt.pcolormesh(
+    #     librosa.frames_to_time(np.arange(spectrogram.shape[1]+1), sr=sr, hop_length=hop_length), 
+    #     np.arange(spectrogram.shape[0]+1), 
+    #     spectrogram,
+    #     cmap='magma'
+    # )
+    # plt.colorbar(format='%+2.0f dB')
+    # # plt.xticks(np.arange(spectrogram.shape[1]+1, 30), np.arange(spectrogram.shape[1]+1, 30))
+    # plt.yticks(np.arange(3, spectrogram.shape[0]+1, 12), ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8'])
+    # plt.title('Spectrogram')
+    # plt.xlabel('Time')
+    # plt.ylabel('Frequency')
+    # plt.show()
+
+    return spectrogram, sr
